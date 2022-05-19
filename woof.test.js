03c21a2eb2f7050ffbb0f59a248cd29c9eafc4ea
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-19 19:35:11 +0200

    added another expects + installed @types/jest to get autocomplete matches

--- woof.test.js
+++ woof.test.js
@@ -3,6 +3,8 @@ const woof = require('./woof');
 test('should return number of woofs', function() {
   const result = woof('oh herro');
   expect(result).toBe('8woof!');
-  expect(result).not.toBe("sadlkhsadklsaj");
-  expect(result).toEqual('8woof!');
+  expect(result).not.toBe('sadlkhsadklsaj');
+  expect({a: 'one', b: 'two'}).toEqual({a: 'one', b: 'two'});
+  expect(result).toMatch(/\dwoof/i);
+  expect(['a', 'b', 'c']).toContain('c');
 });
