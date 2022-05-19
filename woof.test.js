d475b0f90157f35e0867b06fc9efe91b0f7249ae
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-19 19:25:22 +0200

    added some expects

--- woof.test.js
+++ woof.test.js
@@ -1,1 +1,8 @@
-test('our first test', function() {});
+const woof = require('./woof');
+
+test('should return number of woofs', function() {
+  const result = woof('oh herro');
+  expect(result).toBe('8woof!');
+  expect(result).not.toBe("sadlkhsadklsaj");
+  expect(result).toEqual('8woof!');
+});
