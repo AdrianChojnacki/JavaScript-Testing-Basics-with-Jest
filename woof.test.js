629129b01c0c06e70e33424fec725722860c94f9
Adrian Chojnacki (PGS Software) <achojnacki@pgs-soft.com>
2022-05-20 09:51:59 +0200

    added coverage script and covered whole woof.js file in woof.test.js

--- woof.test.js
+++ woof.test.js
@@ -7,4 +7,7 @@ test('should return number of woofs', function() {
   expect({a: 'one', b: 'two'}).toEqual({a: 'one', b: 'two'});
   expect(result).toMatch(/\dwoof/i);
   expect(['a', 'b', 'c']).toContain('c');
+
+  const result2 = woof(42);
+  expect(result2).toBe(undefined);
 });
